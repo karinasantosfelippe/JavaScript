27d78466258f1767032ebb9183959ba809a1841f
Karina Santos Felippe <68240776+karinasantosfelippe@users.noreply.github.com>
2020-07-29 16:51:01 -0300

    Exe 5 OK

--- Rocketseat/ES6/main.js
+++ Rocketseat/ES6/main.js
@@ -1,19 +1,30 @@
-// EXERCICIO 4
+// EXERCICIO 5
 
-const empresa = {
-    nome: "Rocketseat",
-    endereco: {
-        cidade: "Rio do Sul",
-        estado: "SC"
-    }
+// 5.1 PARTE 1
+const arr = [1, 2, 3, 4, 5, 6];
+const [x, ...y] = arr;
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
+
+// 5.1 PARTE 2
+function soma(...nums) {
+  return nums.reduce((a, b) => a + b);
+}
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+
+// 5.2
+const usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    uf: "SC",
+    pais: "Brasil"
+  }
 };
-
-// 4.1
-const {nome, endereco: {cidade, estado}} = empresa;
-console.log(nome); // Rocketseat
-console.log(cidade); // Rio do Sul
-console.log(estado); // SC
-
-// 4.2
-function mostraInfo({ nome, idade }) {return `${nome} tem ${idade} anos.`;}
-mostraInfo({ nome: "Diego", idade: 23 });
+const usuario2 = { ...usuario, nome: "Gabriel" };
+const usuario3 = {
+  ...usuario,
+  endereco: { ...usuario.endereco, cidade: "Lontras" }
+};
